1b520fdad5901569b2fa8a3a3bca67037437a8b2
VictorQ1 <57459323+VictorQ1@users.noreply.github.com>
2019-11-11 15:46:35 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+#comino#
+respuesta=input("how u doin? ")
+if respuesta==("just vibin'"):
+    print("you've passed the vibe check")
+else:
+    print("you failed the vibe check, prepare for unforeseen consequences")
+
+    

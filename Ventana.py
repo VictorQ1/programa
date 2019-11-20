fd9dd4f57ad34b72d688d0418d530345fa407c40
Nalamk2 <57467936+Nalamk2@users.noreply.github.com>
2019-11-20 13:33:42 -0600

    Update Ventana.py

--- Ventana.py
+++ Ventana.py
@@ -10,8 +10,10 @@ top.title('Encuadres')
 top.iconbitmap(r'C:\Users\Alumno 11\Desktop\Encuadre Perron mk 3000\iconochido.ico')
 
 #Funcion correcta
+def Elias():
+   msg = messagebox.showerror("Lo sentimos", "Estamos trabajando en ello")
 def Mandar_Encuadre():
-   msg = messagebox.showinfo("Elias es joto", "Elias es joto")
+   msg = messagebox.showinfo("asd", "asd")   
 def read_from_db():
   c.execute('SELECT * FROM tabla1')
   data = c.fetchall()   
@@ -22,16 +24,16 @@ bottomframe.pack( side = BOTTOM )
 b1 = Button(frame, text = "Matemáticas", fg = "green", command = Mandar_Encuadre,height = '2', width = '10' )
 b1.pack( side = LEFT) 
 
-b2 = Button(frame, text = "Español", fg="blue", command = Mandar_Encuadre,height = '2', width = '10')
+b2 = Button(frame, text = "Español", fg="blue", command = Elias,height = '2', width = '10')
 b2.pack( side = LEFT )
 
-b3 = Button(frame, text = "Deportes", fg = "#6f9dab", command = Mandar_Encuadre,height = '2', width = '10')
+b3 = Button(frame, text = "Deportes", fg = "#6f9dab", command = Elias,height = '2', width = '10')
 b3.pack( side = LEFT )
 
-b4 = Button(bottomframe, text = "Ingles", fg = "purple", command = Mandar_Encuadre,height = '2', width = '10')
+b4 = Button(bottomframe, text = "Ingles", fg = "purple", command = Elias,height = '2', width = '10')
 b4.pack( side = LEFT)
 
-b5 = Button(bottomframe, text = "Historia", fg = "#caae6b", command = Mandar_Encuadre,height = '2', width = '10')
+b5 = Button(bottomframe, text = "Historia", fg = "#caae6b", command = Elias,height = '2', width = '10')
 b5.pack( side = BOTTOM)
 
 
